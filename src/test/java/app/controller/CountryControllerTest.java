d6a180c3166890eb31d52f35ea22d15af4fa570b
Jakub Stanczak <kuba0095@gmail.com>
2016-12-29 13:06:59 +0100

    Add separate db for tests

--- src/test/java/app/controller/CountryControllerTest.java
+++ src/test/java/app/controller/CountryControllerTest.java
@@ -8,6 +8,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.context.WebApplicationContext;
@@ -29,6 +30,7 @@ import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppC
  */
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@ActiveProfiles("test")
 public class CountryControllerTest {
 
     MockMvc mockMvc;
